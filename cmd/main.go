fe7ec0ab4e6a93eaf1e378d73fb44f8e91bd0309
Adam Roman <adam.roman@bergenlogistics.com>
2023-10-08 23:52:55 -0400

    changed endpoint to pong

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
+
+	"github.com/adamnroman/SpeechWriter/pkg/handler"
 )
 
 func main() {
-	// Hello world, the web server
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
-	}
-
-	http.HandleFunc("/hello", helloHandler)
-    log.Println("Listing for requests at http://localhost:8000/hello")
+	http.HandleFunc("/ping", handler.Pong)
+	log.Println("Listing for requests at http://localhost:8000/ping")
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
